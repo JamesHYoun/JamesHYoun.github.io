8a17e9397002f272beb9254bca29abe2917e1c3a
JamesHYoun <james.hc.youn@gmail.com>
2024-08-10 21:29:33 -0700

    Removed code pertaining to SiteForTheTechnical

--- SiteForTheTechnical/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const PORT = process.env.PORT || 3000
-
-const app = require('./src/app')
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
